7c8f699d26def06ba2b4ccbfbc15ac03c64e4b8d
emilie Sirot <emilie.sirot@live.fr>
2021-05-06 10:47:49 +0200

    ajout 2e script avec le masque EXS et suppression de l'extraction du masque EXS dans le script original

---
+++ scripts/applicationMasqueSnow/scriptApplicationMasqueSnow.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  6 10:01:24 2021
+
+@author: sirote
+"""
+
+
+import os
+from pprint import pprint
+from osgeo import gdal
+
+#vérification du système de coordonnées des images et changement de système si besoin
+
