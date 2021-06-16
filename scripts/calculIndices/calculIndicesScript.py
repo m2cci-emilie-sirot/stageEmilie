4e289d2f980e99a7ed8b871dcb293488edf16802
Emilie Sirot <emilie.sirot@live.fr>
2021-06-16 17:34:42 +0200

    avancee script indices sans le indclass

--- scripts/calculIndices/calculIndicesScript.py
+++ scripts/calculIndices/calculIndicesScript.py
@@ -131,40 +131,40 @@ for i in range (len(listeRep)):
                             dst.write(SR.astype(rasterio.float64), 1)
 
               # three bands vegetation indices (3BSI / mSR et 3BSI_Tian)
-            for elt in itertools.permutations(li_band,3):
-                bande1 = elt[0].split("_")[-3]
-                bande2 = elt[1].split("_")[-3]
-                bande3 = elt[2].split("_")[-3]
-                BSI_of = "%s_%s_%s_3BSI_%s.tif" % (bande1, bande2, bande3, x )
-                mSR_of = "%s_%s_%s_mSR_%s.tif" % (bande1, bande2, bande3, x )
-                mSR_of_inv = "%s_%s_%s_mSR_%s.tif" % (bande1, bande3, bande2, x )
-                BSI_Tian_of = "%s_%s_%s_3BSITian_%s.tif" % (bande1, bande2, bande3, x )
-                BSI_Tian_of_inv = "%s_%s_%s_3BSITian_%s.tif" % (bande1, bande3, bande2, x )
-                CVI_of = "%s_%s_%s_CVI_%s.tif" % (bande1, bande2, bande3, x )
-                CVI_of_inv = "%s_%s_%s_CVI_%s.tif" % (bande2, bande1, bande3, x )
-                dst_3BSI=os.path.join(rep_destination_finale1,"3BSI", BSI_of)
-                dst_MSR=os.path.join(rep_destination_finale1,"mSR", mSR_of)
-                dst_MSR_inv=os.path.join(rep_destination_finale1,"mSR", mSR_of_inv)
-                dst_BSI_Tian=os.path.join(rep_destination_finale1,"3BSITian", BSI_Tian_of)
-                dst_BSI_Tian_inv=os.path.join(rep_destination_finale1,"3BSITian", BSI_Tian_of_inv)
-                dst_CVI=os.path.join(rep_destination_finale1,"CVI", CVI_of)
-                dst_CVI_inv=os.path.join(rep_destination_finale1,"CVI", CVI_of_inv)
+            for elt in itertools.permutations(listeBandes,3):
+                bande1 = elt[0].split("_")[3]
+                bande2 = elt[1].split("_")[3]
+                bande3 = elt[2].split("_")[3]
+                BSI_of = "%s_%s_%s_3BSI_%s.tif" % (bande1, bande2, bande3, date )
+                mSR_of = "%s_%s_%s_mSR_%s.tif" % (bande1, bande2, bande3, date )
+                mSR_of_inv = "%s_%s_%s_mSR_%s.tif" % (bande1, bande3, bande2, date )
+                BSI_Tian_of = "%s_%s_%s_3BSITian_%s.tif" % (bande1, bande2, bande3, date )
+                BSI_Tian_of_inv = "%s_%s_%s_3BSITian_%s.tif" % (bande1, bande3, bande2, date )
+                CVI_of = "%s_%s_%s_CVI_%s.tif" % (bande1, bande2, bande3, date )
+                CVI_of_inv = "%s_%s_%s_CVI_%s.tif" % (bande2, bande1, bande3, date )
+                dst_3BSI=os.path.join(repSortie+"/"+listeRep[i],"3BSI", BSI_of)
+                dst_MSR=os.path.join(repSortie+"/"+listeRep[i],"mSR", mSR_of)
+                dst_MSR_inv=os.path.join(repSortie+"/"+listeRep[i],"mSR", mSR_of_inv)
+                dst_BSI_Tian=os.path.join(repSortie+"/"+listeRep[i],"3BSITian", BSI_Tian_of)
+                dst_BSI_Tian_inv=os.path.join(repSortie+"/"+listeRep[i],"3BSITian", BSI_Tian_of_inv)
+                dst_CVI=os.path.join(repSortie+"/"+listeRep[i],"CVI", CVI_of)
+                dst_CVI_inv=os.path.join(repSortie+"/"+listeRep[i],"CVI", CVI_of_inv)
                 if(not os.path.exists(dst_3BSI) or (not os.path.exists(dst_MSR) and not os.path.exists(dst_MSR_inv)) or (not os.path.exists(dst_BSI_Tian) and not os.path.exists(dst_BSI_Tian_inv))or(not os.path.exists(dst_CVI) and not os.path.exists(dst_CVI_inv))):
-                    with rasterio.open(elt[0], "r") as src:
+                    with rasterio.open(repCourant+"/"+listeBandes[0], "r") as src:
                         ba1 = src.read(1)
                         profile = src.profile
                         profile.update(
                                 dtype=rasterio.float64,
                                 count=1,
                                 compress='lzw')
-                    with rasterio.open(elt[1], "r") as src:
+                    with rasterio.open(repCourant+"/"+listeBandes[1], "r") as src:
                         bb1 = src.read(1)
                         profile = src.profile
                         profile.update(
                                 dtype=rasterio.float64,
                                 count=1,
                                 compress='lzw')
-                    with rasterio.open(elt[2], "r") as src:
+                    with rasterio.open(repCourant+"/"+listeBandes[2], "r") as src:
                         bc1 = src.read(1)
                         profile = src.profile
                         profile.update(
@@ -199,160 +199,160 @@ for i in range (len(listeRep)):
                             with rasterio.open(dst_CVI, "w", **profile) as dst:
                                 dst.write(CVI.astype(rasterio.float64), 1)
     ###Création des indices usuels:
-            with rasterio.open(li_band[0], "r") as src:
+            with rasterio.open(repCourant+"/"+listeBandes[0], "r") as src:
                 bandeB2 = src.read(1)
                 profile = src.profile
                 profile.update(
                         dtype=rasterio.float64,
                         count=1,
                         compress='lzw')
-            with rasterio.open(li_band[1], "r") as src:
+            with rasterio.open(repCourant+"/"+listeBandes[1], "r") as src:
                 bandeB3 = src.read(1)
                 profile = src.profile
                 profile.update(
                         dtype=rasterio.float64,
                         count=1,
                         compress='lzw')
-            with rasterio.open(li_band[2], "r") as src:
+            with rasterio.open(repCourant+"/"+listeBandes[2], "r") as src:
                 bandeB4 = src.read(1)
                 profile = src.profile
                 profile.update(
                         dtype=rasterio.float64,
                         count=1,
                         compress='lzw')
-            with rasterio.open(li_band[3], "r") as src:
+            with rasterio.open(repCourant+"/"+listeBandes[3], "r") as src:
                 bandeB5 = src.read(1)
                 profile = src.profile
                 profile.update(
                         dtype=rasterio.float64,
                         count=1,
                         compress='lzw')
-            with rasterio.open(li_band[4], "r") as src:
+            with rasterio.open(repCourant+"/"+listeBandes[4], "r") as src:
                 bandeB6 = src.read(1)
                 profile = src.profile
                 profile.update(
                         dtype=rasterio.float64,
                         count=1,
                         compress='lzw')
-            with rasterio.open(li_band[5], "r") as src:
+            with rasterio.open(repCourant+"/"+listeBandes[5], "r") as src:
                 bandeB7 = src.read(1)
                 profile = src.profile
                 profile.update(
                         dtype=rasterio.float64,
                         count=1,
                         compress='lzw')
-            with rasterio.open(li_band[6], "r") as src:
+            with rasterio.open(repCourant+"/"+listeBandes[6], "r") as src:
                 bandeB8 = src.read(1)
                 profile = src.profile
                 profile.update(
                         dtype=rasterio.float64,
                         count=1,
                         compress='lzw')
-            with rasterio.open(li_band[7], "r") as src:
+            with rasterio.open(repCourant+"/"+listeBandes[7], "r") as src:
                 bandeB8a = src.read(1)
                 profile = src.profile
                 profile.update(
                         dtype=rasterio.float64,
                         count=1,
                         compress='lzw')
-            with rasterio.open(li_band[8], "r") as src:
+            with rasterio.open(repCourant+"/"+listeBandes[8], "r") as src:
                 bandeB11 = src.read(1)
                 profile = src.profile
                 profile.update(
                         dtype=rasterio.float64,
                         count=1,
                         compress='lzw')
-            rep_destination_finale2=os.path.join(rep_destination_finale1,"indclass")
+            # rep_destination_finale2=os.path.join(rep_destination_finale1,"indclass")
             ###################################################################
             #Calcul des indices classiques du fichier .xml
             ###################################################################
             #calcul du NDVI
             NDVI = np.divide((1.0*bandeB8 - bandeB4), (bandeB8 + bandeB4))
             NDVI[np.isinf(NDVI)]=np.nan
-            if not os.path.exists(os.path.join(rep_destination_finale2,"NDVI_indclass_%s.tif"%(x))):
-                with rasterio.open(os.path.join(rep_destination_finale2,"NDVI_indclass_%s.tif"%(x)), "w", **profile) as dst:
+            if not os.path.exists(os.path.join(repSortie+"/"+listeRep[i]+"/"+"indclass","NDVI_indclass_%s.tif"%(date))):
+                with rasterio.open(os.path.join(repSortie+"/"+listeRep[i]+"/"+"indclass","NDVI_indclass_%s.tif"%(date)), "w", **profile) as dst:
                     dst.write(NDVI.astype(rasterio.float64), 1)
 
             #calcul du GNDVI
             GNDVI = np.divide((1.0*bandeB8 - bandeB3), (bandeB8 + bandeB3))
             GNDVI[np.isinf(GNDVI)]=np.nan
-            if not os.path.exists(os.path.join(rep_destination_finale2,"GNDVI_indclass_%s.tif"%(x))):
-                with rasterio.open(os.path.join(rep_destination_finale2,"GNDVI_indclass_%s.tif"%(x)), "w", **profile) as dst:
+            if not os.path.exists(os.path.join(repSortie+"/"+listeRep[i]+"/"+"indclass","GNDVI_indclass_%s.tif"%(date))):
+                with rasterio.open(os.path.join(repSortie+"/"+listeRep[i]+"/"+"indclass","GNDVI_indclass_%s.tif"%(date)), "w", **profile) as dst:
                     dst.write(GNDVI.astype(rasterio.float64), 1)
 
             #calcul du NDVIre
             NDVIre = np.divide((1.0*bandeB8a - bandeB4), (bandeB8a + bandeB4))
             NDVIre[np.isinf(NDVIre)]=np.nan
-            if not os.path.exists(os.path.join(rep_destination_finale2,"NDVIre_indclass_%s.tif"%(x))):
-                with rasterio.open(os.path.join(rep_destination_finale2,"NDVIre_indclass_%s.tif"%(x)), "w", **profile) as dst:
+            if not os.path.exists(os.path.join(repSortie+"/"+listeRep[i]+"/"+"indclass","NDVIre_indclass_%s.tif"%(date))):
+                with rasterio.open(os.path.join(repSortie+"/"+listeRep[i]+"/"+"indclass","NDVIre_indclass_%s.tif"%(date)), "w", **profile) as dst:
                     dst.write(NDVIre.astype(rasterio.float64), 1)
 
             #calcul du NDI45
             NDI45 = np.divide((1.0*bandeB5 - bandeB4), (bandeB5 + bandeB4))
             NDI45[np.isinf(NDI45)]=np.nan
-            if not os.path.exists(os.path.join(rep_destination_finale2,"NDI45_indclass_%s.tif"%(x))):
-                with rasterio.open(os.path.join(rep_destination_finale2,"NDI45_indclass_%s.tif"%(x)), "w", **profile) as dst:
+            if not os.path.exists(os.path.join(repSortie+"/"+listeRep[i]+"/"+"indclass","NDI45_indclass_%s.tif"%(date))):
+                with rasterio.open(os.path.join(repSortie+"/"+listeRep[i]+"/"+"indclass","NDI45_indclass_%s.tif"%(date)), "w", **profile) as dst:
                     dst.write(NDI45.astype(rasterio.float64), 1)
 
             #calcul du NDII
             NDII = np.divide((1.0*bandeB8 - bandeB11), (bandeB8 + bandeB11))
             NDII[np.isinf(NDII)]=np.nan
-            if not os.path.exists(os.path.join(rep_destination_finale2,"NDII_indclass_%s.tif"%(x))):
-                with rasterio.open(os.path.join(rep_destination_finale2,"NDII_indclass_%s.tif"%(x)), "w", **profile) as dst:
+            if not os.path.exists(os.path.join(repSortie+"/"+listeRep[i]+"/"+"indclass","NDII_indclass_%s.tif"%(date))):
+                with rasterio.open(os.path.join(repSortie+"/"+listeRep[i]+"/"+"indclass","NDII_indclass_%s.tif"%(date)), "w", **profile) as dst:
                     dst.write(NDII.astype(rasterio.float64), 1)
 
             #calcul du NREDI1
             NREDI1 = np.divide((1.0*bandeB6 - bandeB5), (bandeB6 + bandeB5))
             NREDI1[np.isinf(NREDI1)]=np.nan
-            if not os.path.exists(os.path.join(rep_destination_finale2,"NREDI1_indclass_%s.tif"%(x))):
-                with rasterio.open(os.path.join(rep_destination_finale2,"NREDI1_indclass_%s.tif"%(x)), "w", **profile) as dst:
+            if not os.path.exists(os.path.join(repSortie+"/"+listeRep[i]+"/"+"indclass","NREDI1_indclass_%s.tif"%(date))):
+                with rasterio.open(os.path.join(repSortie+"/"+listeRep[i]+"/"+"indclass","NREDI1_indclass_%s.tif"%(date)), "w", **profile) as dst:
                     dst.write(NREDI1.astype(rasterio.float64), 1)
 
             #calcul du NREDI2
             NREDI2 = np.divide((1.0*bandeB7 - bandeB5), (bandeB7 + bandeB5))
             NREDI2[np.isinf(NREDI2)]=np.nan
-            if not os.path.exists(os.path.join(rep_destination_finale2,"NREDI2_indclass_%s.tif"%(x))):
-                with rasterio.open(os.path.join(rep_destination_finale2,"NREDI2_indclass_%s.tif"%(x)), "w", **profile) as dst:
+            if not os.path.exists(os.path.join(repSortie+"/"+listeRep[i]+"/"+"indclass","NREDI2_indclass_%s.tif"%(date))):
+                with rasterio.open(os.path.join(repSortie+"/"+listeRep[i]+"/"+"indclass","NREDI2_indclass_%s.tif"%(date)), "w", **profile) as dst:
                     dst.write(NREDI2.astype(rasterio.float64), 1)
 
             #calcul du NREDI3
             NREDI3 = np.divide((1.0*bandeB7 - bandeB6), (bandeB7 + bandeB6))
             NREDI3[np.isinf(NREDI3)]=np.nan
-            if not os.path.exists(os.path.join(rep_destination_finale2,"NREDI3_indclass_%s.tif"%(x))):
-                with rasterio.open(os.path.join(rep_destination_finale2,"NREDI3_indclass_%s.tif"%(x)), "w", **profile) as dst:
+            if not os.path.exists(os.path.join(repSortie+"/"+listeRep[i]+"/"+"indclass","NREDI3_indclass_%s.tif"%(date))):
+                with rasterio.open(os.path.join(repSortie+"/"+listeRep[i]+"/"+"indclass","NREDI3_indclass_%s.tif"%(date)), "w", **profile) as dst:
                     dst.write(NREDI3.astype(rasterio.float64), 1)
 
             #calcul du PSRI
             PSRI = np.divide((1.0*bandeB4 - bandeB3), (bandeB5))
             PSRI[np.isinf(PSRI)]=np.nan
-            if not os.path.exists(os.path.join(rep_destination_finale2,"PSRI_indclass_%s.tif"%(x))):
-                with rasterio.open(os.path.join(rep_destination_finale2,"PSRI_indclass_%s.tif"%(x)), "w", **profile) as dst:
+            if not os.path.exists(os.path.join(repSortie+"/"+listeRep[i]+"/"+"indclass","PSRI_indclass_%s.tif"%(date))):
+                with rasterio.open(os.path.join(repSortie+"/"+listeRep[i]+"/"+"indclass","PSRI_indclass_%s.tif"%(date)), "w", **profile) as dst:
                     dst.write(PSRI.astype(rasterio.float64), 1)
 
             #calcul du MSI
             MSI = np.divide((1.0*bandeB11), (bandeB8))
             MSI[np.isinf(MSI)]=np.nan
-            if not os.path.exists(os.path.join(rep_destination_finale2,"MSI_indclass_%s.tif"%(x))):
-                with rasterio.open(os.path.join(rep_destination_finale2,"MSI_indclass_%s.tif"%(x)), "w", **profile) as dst:
+            if not os.path.exists(os.path.join(repSortie+"/"+listeRep[i]+"/"+"indclass","MSI_indclass_%s.tif"%(date))):
+                with rasterio.open(os.path.join(repSortie+"/"+listeRep[i]+"/"+"indclass","MSI_indclass_%s.tif"%(date)), "w", **profile) as dst:
                     dst.write(MSI.astype(rasterio.float64), 1)
 
             #calcul du IRECI
             IRECI = np.divide((1.0*bandeB7 - bandeB4), (np.divide(1.0*bandeB5 , bandeB6)))
             IRECI[np.isinf(IRECI)]=np.nan
-            if not os.path.exists(os.path.join(rep_destination_finale2,"IRECI_indclass_%s.tif"%(x))):
-                with rasterio.open(os.path.join(rep_destination_finale2,"IRECI_indclass_%s.tif"%(x)), "w", **profile) as dst:
+            if not os.path.exists(os.path.join(repSortie+"/"+listeRep[i]+"/"+"indclass","IRECI_indclass_%s.tif"%(date))):
+                with rasterio.open(os.path.join(repSortie+"/"+listeRep[i]+"/"+"indclass","IRECI_indclass_%s.tif"%(date)), "w", **profile) as dst:
                     dst.write(IRECI.astype(rasterio.float64), 1)
 
             #calcul du MTCI
             MTCI = np.divide((1.0*bandeB8 - bandeB5), (bandeB5 - bandeB4))
             MTCI[np.isinf(MTCI)]=np.nan
-            if not os.path.exists(os.path.join(rep_destination_finale2,"MTCI_indclass_%s.tif"%(x))):
-                with rasterio.open(os.path.join(rep_destination_finale2,"MTCI_indclass_%s.tif"%(x)), "w", **profile) as dst:
+            if not os.path.exists(os.path.join(repSortie+"/"+listeRep[i]+"/"+"indclass","MTCI_indclass_%s.tif"%(date))):
+                with rasterio.open(os.path.join(repSortie+"/"+listeRep[i]+"/"+"indclass","MTCI_indclass_%s.tif"%(date)), "w", **profile) as dst:
                     dst.write(MTCI.astype(rasterio.float64), 1)
 
             #calcul du MCARI
             MCARI = np.multiply(((bandeB5 - bandeB4) - (0.2*(bandeB5 - bandeB3))), (bandeB5 - bandeB4))
             MCARI[np.isinf(MCARI)]=np.nan
-            if not os.path.exists(os.path.join(rep_destination_finale2,"MCARI_indclass_%s.tif"%(x))):
-                with rasterio.open(os.path.join(rep_destination_finale2,"MCARI_indclass_%s.tif"%(x)), "w", **profile) as dst:
+            if not os.path.exists(os.path.join(repSortie+"/"+listeRep[i]+"/"+"indclass","MCARI_indclass_%s.tif"%(date))):
+                with rasterio.open(os.path.join(repSortie+"/"+listeRep[i]+"/"+"indclass","MCARI_indclass_%s.tif"%(date)), "w", **profile) as dst:
                     dst.write(MCARI.astype(rasterio.float64), 1)
