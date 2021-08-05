e1d0e3a05fd7c51352a815c6af5a79f96f930887
Emilie Sirot <emilie.sirot@live.fr>
2021-08-05 18:36:13 +0200

    avancee script graphique medianes

--- scripts/graphique/graphique.py
+++ scripts/graphique/graphique.py
@@ -31,20 +31,63 @@ for i in range (len(listeRep)):
 
     csvfile = pd.read_csv(os.path.join(repDonnees,fichiersRep[0]), header=0)
    
-    colonnes = len(csvfile.columns)
-    
+     
     listeMEDI = []
+    listeLAND = []
+    listeSUB = []
+    listeBOIS = []
+    listeECOR = []
+    listePROD = []
+    
+    
+    row = 3
     
     for c in range(3, len(csvfile.columns)):
-        if csvfile.iloc[2,i] = "MEDI":
-            listeMEDI.append(csvfile.iloc[2,i])
+        if csvfile.iloc[2,c] == "MEDI":
+            listeMEDI.append(csvfile.iloc[row,c])
             
+            
+        elif csvfile.iloc[2,c] == "LAND":
+            listeLAND.append(csvfile.iloc[row,c])
+            
+            
+        elif csvfile.iloc[2,c] == "SUB":
+            listeSUB.append(csvfile.iloc[row,c])
+            
+            
+        elif csvfile.iloc[2,c] == "BOIS":
+            listeBOIS.append(csvfile.iloc[row,c])
+            
+            
+        elif csvfile.iloc[2,c] == "ECOR":
+            listeECOR.append(csvfile.iloc[row,c])
+            
+            
+        elif csvfile.iloc[2,c] == "PROD":
+            listePROD.append(csvfile.iloc[row,c])
+            
+   
+            
+            
+    listeMEDIFloat = list(map(float, listeMEDI))
+    listeLANDFloat = list(map(float, listeLAND))
+    listeSUBFloat = list(map(float, listeSUB))
+    listeBOISFloat = list(map(float, listeBOIS))
+    listeECORFloat = list(map(float, listeECOR))
+    listePRODFloat = list(map(float, listePROD))
     
    
+    medianeMEDI = statistics.median(listeMEDIFloat)
+    medianeLAND = statistics.median(listeLANDFloat)
+    medianeSUB = statistics.median(listeSUBFloat)
+    medianeBOIS = statistics.median(listeBOISFloat)
+    medianeECOR = statistics.median(listeECORFloat)
+    medianePROD = statistics.median(listePRODFloat)
     
-   
-   
-    mediane = statistics.median(lst)
+    
+    
+    
+    
     
    test = csvfile["date"].values
     plt.figure()
